934b48774792ae870b0a16f812f5e6ae6547ca59
MrJwiz <misterjwiz@gmail.com>
2016-01-09 11:37:05 -0800

    Revert "[1/2] Frameworks: Add per app controls for keyguard notifications"
    This reverts commit fb0c3ae95971c4bfb24dfeba07a72436d397a778.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -209,8 +209,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected WindowManager mWindowManager;
     protected IWindowManager mWindowManagerService;
 
-    private NotificationManager mNoMan;
-
     protected abstract void refreshLayout(int layoutDirection);
 
     protected Display mDisplay;
@@ -411,7 +409,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                     }
                 }
             } else if (BANNER_ACTION_CANCEL.equals(action) || BANNER_ACTION_SETUP.equals(action)) {
-                mNoMan.cancel(R.id.notification_hidden);
+                NotificationManager noMan = (NotificationManager)
+                        mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                noMan.cancel(R.id.notification_hidden);
 
                 Settings.Secure.putInt(mContext.getContentResolver(),
                         Settings.Secure.SHOW_NOTE_ABOUT_NOTIFICATION_HIDING, 0);
@@ -540,7 +540,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     public void start() {
         mWindowManager = (WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE);
         mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
-        mNoMan = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         mDisplay = mWindowManager.getDefaultDisplay();
         mDevicePolicyManager = (DevicePolicyManager)mContext.getSystemService(
                 Context.DEVICE_POLICY_SERVICE);
@@ -705,7 +704,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                             mContext.getString(R.string.hidden_notifications_setup),
                             setupIntent);
 
-            mNoMan.notify(R.id.notification_hidden, note.build());
+            NotificationManager noMan =
+                    (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            noMan.notify(R.id.notification_hidden, note.build());
         }
     }
 
@@ -1864,16 +1865,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private boolean shouldShowOnKeyguard(StatusBarNotification sbn) {
-        final int showOnKeyguard = mNoMan.getShowNotificationForPackageOnKeyguard(
-                sbn.getPackageName(), sbn.getUid());
-        boolean isKeyguardAllowedForApp =
-                (showOnKeyguard & Notification.SHOW_ALL_NOTI_ON_KEYGUARD) != 0;
-        if (isKeyguardAllowedForApp && sbn.isOngoing()) {
-            isKeyguardAllowedForApp =
-                    (showOnKeyguard & Notification.SHOW_NO_ONGOING_NOTI_ON_KEYGUARD) == 0;
-        }
-        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey())
-                && isKeyguardAllowedForApp;
+        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey());
     }
 
     private void hideWeatherPanelIfNecessary(int visibleNotifications, int maxKeyguardNotifications) {
