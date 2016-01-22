e2e894d13f7f9f60e295c7e0daea3a90d34f188d
LorDClockaN <davor@losinj.com>
2016-01-21 19:21:23 -0500

    FWB: QS header transparency (1/2)
    Change-Id: Ibe86f4405433b46c5e36c4e4d0fad788a5e65724

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4225,6 +4225,12 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Transparent qs header
+         * @hide
+         */
+        public static final String QS_TRANSPARENT_HEADER = "qs_transparent_header";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
